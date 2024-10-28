956aae64d7c8ce41b46bb42f8a7ac1cd2c2b9e08
Lautaro Moyano <lautaromoyanoing@gmail.com>
2024-10-27 23:57:07 -0300

    Pusheo alta turnos funcionando

--- Menu.pyw
+++ Menu.pyw
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import PhotoImage, Toplevel
 from viewpersonas import ViewPersonas  # Importamos la clase ViewPersonas
 from viewserviciosSEGUNDO import VerServicios
+from alta_turnos import iniciar_turnero
 
 class Menu(tk.Tk):
     def __init__(self):
@@ -12,7 +13,7 @@ class Menu(tk.Tk):
         self.title("Menu")
 
         # Cargar imagen
-        ruta_imagen = 'C:/Users/GUILLERMINA/Desktop/Interfaces_Peluqueria/imagen3.png'
+        ruta_imagen = 'C:/Users/lauta/OneDrive/Desktop/Facultad/Interfaces_Peluqueria/imagen4.png'
         self.imagen = PhotoImage(file=ruta_imagen)
 
         # Etiqueta para mostrar la imagen
@@ -24,15 +25,13 @@ class Menu(tk.Tk):
         button_frame.grid(row=1, column=0, columnspan=4, padx=20, pady=10, sticky="ew")
 
         # Botones
-        tk.Button(button_frame, text="Turnos", font=("Calibri", 15),width=8, command=self.turnos).grid(row=0, column=0, padx=30)
+        tk.Button(button_frame, text="Turnos", font=("Calibri", 15),width=8, command=self.abrir_gestion_turnos).grid(row=0, column=0, padx=30)
         tk.Button(button_frame, text="Personas", font=("Calibri", 15),width=8, command=self.personas).grid(row=0, column=1, padx=30)
         tk.Button(button_frame, text="Stock", font=("Calibri", 15),width=8 ,command=self.stock).grid(row=0, column=2, padx=30)
         tk.Button(button_frame, text="Servicios", font=("Calibri", 15),width=8, command=self.servicios).grid(row=0, column=3, padx=30)
 
-    def turnos(self):
-        ventana_turnos = Toplevel(self)
-        ventana_turnos.title("Turnos")
-        # Aquí puedes agregar contenido a la ventana de turnos
+    def abrir_gestion_turnos(self):
+        iniciar_turnero()
 
     def personas(self):
         ventana_personas = Toplevel(self)
--- alta_turnos.py
+++ alta_turnos.py
@@ -6,222 +6,227 @@ from tkinter import messagebox
 from datetime import datetime  # Importar datetime
 from PIL import Image, ImageTk  # Importar PIL para manejar imágenes
 
-# Conexión a la base de datos
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="123",
-    database="base_peluqueria"
-)
-cursor = db.cursor()
 
-# Función para mostrar turnos filtrados por fecha
-def mostrar_turnos(fecha=None):
-    # Limpiar el Treeview
-    for row in tree.get_children():
-        tree.delete(row)
+def iniciar_turnero():
+    # Conexión a la base de datos
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123",
+        database="base_peluqueria"
+    )
+    cursor = db.cursor()
 
-    # Asegurarse de que no haya resultados no leídos
-    if cursor.stored_results():
-        cursor.fetchall()  # Descartar cualquier resultado no leído
-
-    # Ejecutar la nueva consulta
-    if fecha:
-        cursor.execute("SELECT fecha, hora, nombre_cliente, nombre FROM turno JOIN servicio ON turno.id_servicio = servicio.id_servicio WHERE fecha = %s ORDER BY fecha DESC", (fecha,))
-    else:
-        cursor.execute("SELECT fecha, hora, nombre_cliente, nombre FROM turno JOIN servicio ON turno.id_servicio = servicio.id_servicio ORDER BY fecha DESC")
-    
-    turnos = cursor.fetchall()
-    
-    for turno in turnos:
-        tree.insert("", "end", values=turno)
-
-# Función para filtrar turnos por la fecha seleccionada
-def filtrar_por_fecha():
-    fecha_seleccionada = cal.get_date()
-    mostrar_turnos(fecha_seleccionada)
-
-def registrar_turno():
-    fecha = cal.get_date()
-    hora = entry_hora.get()
-    nombre_cliente = entry_cliente.get()
-    servicio = combo_servicio.get()
-
-    # Validar que la fecha no sea anterior a hoy
-    fecha_actual = datetime.now().date()
-    fecha_turno = datetime.strptime(fecha, "%Y-%m-%d").date()
-
-    if fecha_turno < fecha_actual:
-        messagebox.showerror("Error", "No se pueden registrar turnos en fechas anteriores a hoy.")
-        return
-
-    # Obtener el id del servicio seleccionado
-    cursor.execute("SELECT id_servicio FROM servicio WHERE nombre = %s", (servicio,))
-    id_servicio = cursor.fetchone()
-
-    if id_servicio:
-        cursor.execute(
-            "INSERT INTO turno (fecha, hora, id_servicio, nombre_cliente) VALUES (%s, %s, %s, %s)",
-            (fecha, hora, id_servicio[0], nombre_cliente)
-        )
-        db.commit()
-        messagebox.showinfo("Éxito", "Turno registrado correctamente")
-        mostrar_turnos()
-    else:
-        messagebox.showerror("Error", "Servicio no encontrado")
-
-def cargar_turno_seleccionado(event):
-    selected_item = tree.selection()
-    if selected_item:
-        item_values = tree.item(selected_item, 'values')
-        entry_hora.delete(0, tk.END)
-        entry_hora.insert(0, item_values[1])  # Hora
-        entry_cliente.delete(0, tk.END)
-        entry_cliente.insert(0, item_values[2])  # Nombre del Cliente
-        combo_servicio.set(item_values[3])  # Servicio
-        global turno_id
+    # Función para mostrar turnos filtrados por fecha
+    def mostrar_turnos(fecha=None):
+        # Limpiar el Treeview
+        for row in tree.get_children():
+            tree.delete(row)
 
         # Asegurarse de que no haya resultados no leídos
         if cursor.stored_results():
             cursor.fetchall()  # Descartar cualquier resultado no leído
 
-        # Obtener el id del turno seleccionado
-        cursor.execute("SELECT id_turno FROM turno WHERE fecha = %s AND hora = %s AND nombre_cliente = %s", (item_values[0], item_values[1], item_values[2]))
-        turno_id = cursor.fetchone()[0]  # Guardar el ID del turno seleccionado
-def modificar_turno():
-    if not turno_id:
-        messagebox.showerror("Error", "No se ha seleccionado ningún turno para modificar.")
-        return
+        # Ejecutar la nueva consulta
+        if fecha:
+            cursor.execute("SELECT fecha, hora, nombre_cliente, nombre FROM turno JOIN servicio ON turno.id_servicio = servicio.id_servicio WHERE fecha = %s ORDER BY fecha DESC", (fecha,))
+        else:
+            cursor.execute("SELECT fecha, hora, nombre_cliente, nombre FROM turno JOIN servicio ON turno.id_servicio = servicio.id_servicio ORDER BY fecha DESC")
+        
+        turnos = cursor.fetchall()
+        
+        for turno in turnos:
+            tree.insert("", "end", values=turno)
 
-    fecha = cal.get_date()
-    hora = entry_hora.get()
-    nombre_cliente = entry_cliente.get()
-    servicio = combo_servicio.get()
+    # Función para filtrar turnos por la fecha seleccionada
+    def filtrar_por_fecha():
+        fecha_seleccionada = cal.get_date()
+        mostrar_turnos(fecha_seleccionada)
 
-    # Obtener el id del servicio seleccionado
-    cursor.execute("SELECT id_servicio FROM servicio WHERE nombre = %s", (servicio,))
-    id_servicio = cursor.fetchone()
+    def registrar_turno():
+        fecha = cal.get_date()
+        hora = entry_hora.get()
+        nombre_cliente = entry_cliente.get()
+        servicio = combo_servicio.get()
 
-    if id_servicio:
-        cursor.execute(
-            "UPDATE turno SET fecha = %s, hora = %s, id_servicio = %s, nombre_cliente = %s WHERE id_turno = %s",
-            (fecha, hora, id_servicio[0], nombre_cliente, turno_id)
-        )
-        db.commit()
-        messagebox.showinfo("Éxito", "Turno modificado correctamente")
-        mostrar_turnos()
-    else:
-        messagebox.showerror("Error", "Servicio no encontrado")
+        # Validar que todos los campos estén llenos
+        if not fecha or not hora or not nombre_cliente or not servicio:
+            messagebox.showerror("Error", "Todos los campos deben estar completos.")
+            return
 
-def eliminar_turno():
-    global turno_id
-    if not turno_id:
-        messagebox.showerror("Error", " No se ha seleccionado ningún turno para eliminar.")
-        return
+        # Convertir los valores a mayúsculas
+        nombre_cliente = nombre_cliente.upper()
+        servicio = servicio.upper()
+        
+        # Validar que la fecha no sea anterior a hoy
+        fecha_actual = datetime.now().date()
+        fecha_turno = datetime.strptime(fecha, "%Y-%m-%d").date()
 
-    # Confirmar la eliminación
-    confirm = messagebox.askyesno("Confirmar", "¿Estás seguro de que deseas eliminar este turno?")
-    if confirm:
-        cursor .execute("DELETE FROM turno WHERE id_turno = %s", (turno_id,))
-        db.commit()
-        messagebox.showinfo("Éxito", "Turno eliminado correctamente")
-        mostrar_turnos()
-        turno_id = None
-def volver():
-    # Aquí puedes implementar la lógica para volver a la vista anterior
-    # Por ejemplo, si estás en un formulario, puedes cerrarlo o limpiar los campos
-    entry_hora.delete(0, tk.END)
-    entry_cliente.delete(0, tk.END)
-    combo_servicio.set('')
-    mostrar_turnos()  # Volver a mostrar la lista de turnos
+        if fecha_turno < fecha_actual:
+            messagebox.showerror("Error", "No se pueden registrar turnos en fechas anteriores a hoy.")
+            return
 
-# Crear la ventana principal
-root = tk.Tk()
-root.resizable(False, False)  # Deshabilitar el redimensionamiento de la ventana
-root.title("Sistema de Turnos de Peluquería")
-root.configure(background='#008B8B')  # Establecer el color de fondo de la ventana principal
+        # Obtener el id del servicio seleccionado
+        cursor.execute("SELECT id_servicio FROM servicio WHERE UPPER(nombre) = %s", (servicio,))
+        id_servicio = cursor.fetchone()
 
-# Sección izquierda : Ingreso de datos
-frame_left = tk.Frame(root, bg='#66CCCC')  # Establecer el color de fondo del frame izquierdo
-frame_left.pack(side=tk.LEFT, padx=10, pady=10)
+        if id_servicio:
+            cursor.execute(
+                "INSERT INTO turno (fecha, hora, id_servicio, nombre_cliente) VALUES (%s, %s, %s, %s)",
+                (fecha, hora, id_servicio[0], nombre_cliente)
+            )
+            db.commit()
+            messagebox.showinfo("Éxito", "Turno registrado correctamente")
+            mostrar_turnos()
+        else:
+            messagebox.showerror("Error", "Servicio no encontrado")
 
-label_fecha = tk.Label(frame_left, text="Fecha:", bg='#66CCCC')  # Establecer el color de fondo del label
-label_fecha.pack()
-cal = Calendar(frame_left, date_pattern='y-mm-dd', background='#66CCCC', foreground='black', borderwidth=1)
-cal.pack(pady=5)
+    def cargar_turno_seleccionado(event):
+        selected_item = tree.selection()
+        if selected_item:
+            item_values = tree.item(selected_item, 'values')
+            entry_hora.delete(0, tk.END)
+            entry_hora.insert(0, item_values[1])  # Hora
+            entry_cliente.delete(0, tk.END)
+            entry_cliente.insert(0, item_values[2])  # Nombre del Cliente
+            combo_servicio.set(item_values[3])  # Servicio
+            global turno_id
 
-# Mover el botón de filtrar debajo del calendario
-button_filtrar = tk.Button(frame_left, text="Filtrar Turnos por Fecha", command=filtrar_por_fecha, bg='#F7F7F7')  # Establecer el color de fondo del botón
-button_filtrar.pack(pady=10)
+            # Asegurarse de que no haya resultados no leídos
+            if cursor.stored_results():
+                cursor.fetchall()  # Descartar cualquier resultado no leído
 
-label_hora = tk.Label(frame_left, text="Hora (HH:MM):", bg='#66CCCC')  # Establecer el color de fondo del label
-label_hora.pack()
+            # Obtener el id del turno seleccionado
+            cursor.execute("SELECT id_turno FROM turno WHERE fecha = %s AND hora = %s AND nombre_cliente = %s", (item_values[0], item_values[1], item_values[2]))
+            turno_id = cursor.fetchone()[0]  # Guardar el ID del turno seleccionado
+    def modificar_turno():
+        global turno_id
+        if not turno_id:
+            messagebox.showerror("Error", "No se ha seleccionado ningún turno para modificar.")
+            return
 
-entry_hora = tk.Entry(frame_left)
-entry_hora.insert(0, "HH:MM")  # Establecer el valor inicial
-entry_hora.pack(pady=5)
+        fecha = cal.get_date()
+        hora = entry_hora.get().upper()  # Convertir hora a mayúsculas
+        nombre_cliente = entry_cliente.get().upper()  # Convertir nombre_cliente a mayúsculas
+        servicio = combo_servicio.get().upper()  # Convertir servicio a mayúsculas
 
-label_cliente = tk.Label(frame_left, text="Nombre del Cliente:", bg='#66CCCC')  # Establecer el color de fondo del label
-label_cliente.pack()
-entry_cliente = tk.Entry(frame_left)
-entry_cliente.pack(pady=5)
+        # Obtener el id del servicio seleccionado
+        cursor.execute("SELECT id_servicio FROM servicio WHERE UPPER(nombre) = %s", (servicio,))
+        id_servicio = cursor.fetchone()
 
-label_servicio = tk.Label(frame_left, text="Servicio:", bg='#66CCCC')  # Establecer el color de fondo del label
-label_servicio.pack()
-combo_servicio = ttk.Combobox(frame_left)
-combo_servicio.pack(pady=5)
+        if id_servicio:
+            cursor.execute(
+                "UPDATE turno SET fecha = %s, hora = %s, id_servicio = %s, nombre_cliente = %s WHERE id_turno = %s",
+                (fecha, hora, id_servicio[0], nombre_cliente, turno_id)
+            )
+            db.commit()
+            messagebox.showinfo("Éxito", "Turno modificado correctamente")
+            mostrar_turnos()
+        else:
+            messagebox.showerror("Error", "Servicio no encontrado")
+    def eliminar_turno():
+        global turno_id
+        if not turno_id:
+            messagebox.showerror("Error", " No se ha seleccionado ningún turno para eliminar.")
+            return
 
-# Obtener los servicios disponibles de la base de datos
-cursor.execute("SELECT nombre FROM servicio")
-servicios = cursor.fetchall()
-combo_servicio['values'] = [servicio[0] for servicio in servicios]
+        # Confirmar la eliminación
+        confirm = messagebox.askyesno("Confirmar", "¿Estás seguro de que deseas eliminar este turno?")
+        if confirm:
+            cursor .execute("DELETE FROM turno WHERE id_turno = %s", (turno_id,))
+            db.commit()
+            messagebox.showinfo("Éxito", "Turno eliminado correctamente")
+            mostrar_turnos()
+            turno_id = None
 
-# Sección derecha: Grilla de turnos
-frame_right = tk.Frame(root, bg='#66CCCC')  # Establecer el color de fondo del frame derecho
-frame_right.pack(side=tk.RIGHT, padx=10, pady=10)
 
-label_turnos = tk.Label(frame_right, text="Turnos Registrados", font=("Arial", 20), bg='#66CCCC')  # Establecer el color de fondo del label y aumentar el tamaño de la fuente
-label_turnos.pack(pady=5)
+    # Crear la ventana principal
+    root = tk.Tk()
+    root.resizable(False, False)  # Deshabilitar el redimensionamiento de la ventana
+    root.title("Sistema de Turnos de Peluquería")
+    root.configure(background='#008B8B')  # Establecer el color de fondo de la ventana principal
 
-# Configuración de la grilla (Treeview)
-columns = ("Fecha", "Hora", "Nombre Cliente", "Servicio")
-tree = ttk.Treeview(frame_right, columns=columns, show='headings', selectmode='browse')
-tree.heading("Fecha", text="Fecha")
-tree.heading("Hora", text="Hora")
-tree.heading("Nombre Cliente", text="Nombre Cliente")
-tree.heading("Servicio", text="Servicio")
-tree.pack(pady=10)
+    # Sección izquierda : Ingreso de datos
+    frame_left = tk.Frame(root, bg='#66CCCC')  # Establecer el color de fondo del frame izquierdo
+    frame_left.pack(side=tk.LEFT, padx=10, pady=10)
 
-# Asignar evento de selección de fila
-tree.bind("<<TreeviewSelect>>", cargar_turno_seleccionado)
+    label_fecha = tk.Label(frame_left, text="Fecha:", bg='#66CCCC')  # Establecer el color de fondo del label
+    label_fecha.pack()
+    cal = Calendar(frame_left, date_pattern='y-mm-dd', background='#66CCCC', foreground='black', borderwidth=1)
+    cal.pack(pady=5)
 
-# Frame para botones
-frame_botones = tk.Frame(frame_right, bg='#66CCCC')  # Establecer el color de fondo del frame de botones
-frame_botones.pack(pady=20)
+    # Mover el botón de filtrar debajo del calendario
+    button_filtrar = tk.Button(frame_left, text="Filtrar Turnos por Fecha", command=filtrar_por_fecha, bg='#F7F7F7')  # Establecer el color de fondo del botón
+    button_filtrar.pack(pady=10)
 
-# Botones
-button_registrar = tk.Button(frame_botones, text="Registrar Turno", command=registrar_turno, bg='#F7F7F7')  # Establecer el color de fondo del botón
-button_registrar.pack(side=tk.LEFT, padx=10)
+    label_hora = tk.Label(frame_left, text="Hora (HH:MM):", bg='#66CCCC')  # Establecer el color de fondo del label
+    label_hora.pack()
 
-button_modificar = tk.Button(frame_botones, text="Modificar Turno", command=modificar_turno, bg='#F7F7F7')  # Establecer el color de fondo del botón
-button_modificar.pack(side=tk.LEFT, padx=10)
+    entry_hora = tk.Entry(frame_left)
+    entry_hora.insert(0, "HH:MM")  # Establecer el valor inicial
+    entry_hora.pack(pady=5)
 
-button_eliminar = tk.Button(frame_botones, text="Eliminar Turno", command=eliminar_turno, bg='#F7F7F7')  # Establecer el color de fondo del botón
-button_eliminar.pack(side=tk.LEFT, padx=10)
-# Botón Volver
-button_volver = tk.Button(frame_botones, text="Volver", command=volver, bg='#F7F7F7')  # Establecer el color de fondo del botón
-button_volver.pack(side=tk.LEFT, padx=10)
+    label_cliente = tk.Label(frame_left, text="Nombre del Cliente:", bg='#66CCCC')  # Establecer el color de fondo del label
+    label_cliente.pack()
+    entry_cliente = tk.Entry(frame_left)
+    entry_cliente.pack(pady=5)
 
-# Logo de la peluquería
-image = Image.open("C:/Users/lauta/OneDrive/Desktop/Facultad/Interfaces_Peluqueria/imagen3.png")
-photo = ImageTk.PhotoImage(image)
-logo_label = tk.Label(frame_right, image=photo, bg='#66CCCC')  # Establecer el color de fondo del label
-logo_label.image = photo
-logo_label.pack(side=tk.BOTTOM, anchor=tk.SE, padx=10, pady=10)
+    label_servicio = tk.Label(frame_left, text="Servicio:", bg='#66CCCC')  # Establecer el color de fondo del label
+    label_servicio.pack()
+    combo_servicio = ttk.Combobox(frame_left)
+    combo_servicio.pack(pady=5)
 
-# Mostrar todos los turnos al iniciar
-mostrar_turnos()
+    # Obtener los servicios disponibles de la base de datos
+    cursor.execute("SELECT nombre FROM servicio")
+    servicios = cursor.fetchall()
+    combo_servicio['values'] = [servicio[0] for servicio in servicios]
 
-turno_id = None
+    # Sección derecha: Grilla de turnos
+    frame_right = tk.Frame(root, bg='#66CCCC')  # Establecer el color de fondo del frame derecho
+    frame_right.pack(side=tk.RIGHT, padx=10, pady=10)
 
-root.mainloop()
+    label_turnos = tk.Label(frame_right, text="Turnos Registrados", font=("Arial", 20), bg='#66CCCC')  # Establecer el color de fondo del label y aumentar el tamaño de la fuente
+    label_turnos.pack(pady=5)
+
+    # Configuración de la grilla (Treeview)
+    columns = ("Fecha", "Hora", "Nombre Cliente", "Servicio")
+    tree = ttk.Treeview(frame_right, columns=columns, show='headings', selectmode='browse')
+    tree.heading("Fecha", text="Fecha")
+    tree.heading("Hora", text="Hora")
+    tree.heading("Nombre Cliente", text="Nombre Cliente")
+    tree.heading("Servicio", text="Servicio")
+    tree.pack(pady=10)
+
+    # Asignar evento de selección de fila
+    tree.bind("<<TreeviewSelect>>", cargar_turno_seleccionado)
+
+    # Frame para botones
+    frame_botones = tk.Frame(frame_right, bg='#66CCCC')  # Establecer el color de fondo del frame de botones
+    frame_botones.pack(pady=20)
+
+    # Botones
+    button_registrar = tk.Button(frame_botones, text="Registrar Turno", command=registrar_turno, bg='#F7F7F7')  # Establecer el color de fondo del botón
+    button_registrar.pack(side=tk.LEFT, padx=10)
+
+    button_modificar = tk.Button(frame_botones, text="Modificar Turno", command=modificar_turno, bg='#F7F7F7')  # Establecer el color de fondo del botón
+    button_modificar.pack(side=tk.LEFT, padx=10)
+
+    button_eliminar = tk.Button(frame_botones, text="Eliminar Turno", command=eliminar_turno, bg='#F7F7F7')  # Establecer el color de fondo del botón
+    button_eliminar.pack(side=tk.LEFT, padx=10)
+    # Botón Volver
+    button_volver = tk.Button(frame_botones, text="Volver", command=root.destroy, bg='#F7F7F7')  # Establecer el color de fondo del botón
+    button_volver.pack(side=tk.LEFT, padx=10)
+
+    # Logo de la peluquería
+    image = Image.open("C:/Users/lauta/OneDrive/Desktop/Facultad/Interfaces_Peluqueria/imagen3.png")
+    photo = ImageTk.PhotoImage(image)
+    logo_label = tk.Label(frame_right, image=photo, bg='#66CCCC')  # Establecer el color de fondo del label
+    logo_label.image = photo
+    logo_label.pack(side=tk.BOTTOM, anchor=tk.SE, padx=10, pady=10)
+
+    # Mostrar todos los turnos al iniciar
+    mostrar_turnos()
+
+    turno_id = None
+
+    root.mainloop()
